2127687af95f0df3af64dc7337834fa4ac150b83
mehdi <mehdimoit@gmail.com>
2026-04-21 20:45:33 +0200

    changing task list view function to a class view

--- tasks/views.py
+++ tasks/views.py
@@ -1,25 +1,50 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
+from django.views import View
 
-@login_required
-def tasks_list(request):
 
-    if request.method == 'POST':
+
+class TaskListView(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request):
+        tasks = Task.objects.filter(user=request.user).order_by('-status', 'date_time_created')
+        editing_id = request.GET.get('edit')
+        if editing_id:
+            editing_id = int(editing_id)
+        return render(request, 'tasks/tasks_list.html', {
+            'tasks':tasks,
+            'editing_id':editing_id})
+        
+    
+    def post(self, request):
         body = request.POST.get('body')
         if body:
-            Task.objects.create(body=body, user=request.user)
+            Task.objects.create(body = body, user=request.user)
         return redirect('/')
 
-    tasks = Task.objects.filter(user=request.user).order_by('-status', '-date_time_created')
-    editing_id = request.GET.get('edit')  # check if user clicked edit on a task
-    if editing_id:
-        editing_id = int(editing_id)
+    
 
-    return render(request, 'tasks/tasks_list.html', {
-        'tasks':tasks, 
-        'editing_id':editing_id
-        })
+# @login_required
+# def tasks_list(request):
+
+#     if request.method == 'POST':
+#         body = request.POST.get('body')
+#         if body:
+#             Task.objects.create(body=body, user=request.user)
+#         return redirect('/')
+
+#     tasks = Task.objects.filter(user=request.user).order_by('-status', '-date_time_created')
+#     editing_id = request.GET.get('edit')  # check if user clicked edit on a task
+#     if editing_id:
+#         editing_id = int(editing_id)
+
+#     return render(request, 'tasks/tasks_list.html', {
+#         'tasks':tasks, 
+#         'editing_id':editing_id
+#         })
 
 @login_required
 def task_edit(request, task_id):
